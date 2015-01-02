7346d618bff80c7e218222923c21d77b460e9978
Tamara Temple <tamouse@gmail.com>
2015-01-02 10:11:56 -0600

    remove .emacs.d folder (separate repo), add some aliases, general cleanup

--- emacs.d/tamara/erc-config.el
+++
@@ -1,8 +0,0 @@
-(setq erc-hide-list '("JOIN" "PART" "QUIT"))
-(setq erc-kill-buffer-on-part t)
-(setq erc-kill-queries-on-quit t)
-
-(global-set-key "\C-c\C-ef" (lambda () (interactive)
-                           (erc :server "irc.freenode.net" :port "6667"
-                                :nick "pontiki" :password "nitwood"
-                                :full-name "Tamara Temple <tamouse@gmail.com>")))
