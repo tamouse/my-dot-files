a834fb577fb96ae8602e7d23840dd4dce0f13923
Tamara Temple <tamouse@gmail.com>
2016-08-23 07:53:53 -0500

    Bluewater vsc function and aliases

--- bashrc.d/bluewater-fission.sh
+++ bashrc.d/bluewater-fission.sh
@@ -1,9 +1,16 @@
 # aliases for bluewater fission project
 
-alias vsrci="vagrant ssh -c 'cd sync && rake db:mongoid:create_indexes'"
-alias vsrrad="vagrant ssh -c 'cd sync && rails runner \"AdminUser.create(email: %q(development@bluewaterbrand.com), password: %q(password!!!), password_confirmation: %q(password!!!))\"'"
-alias vsrs="vagrant ssh -c 'cd sync && rails s -b 0.0.0.0'"
-alias vstp="vagrant ssh -c 'cd sync && rake db:test:prepare'"
-alias vstest="vagrant ssh -c 'cd sync && rake db:test:prepare && RAILS_ENV=test rake db:mongoid:create_indexes && xvfb-run rspec && xvfb-run cucumber --tags ~@skip'"
-alias vsspec="vagrant ssh -c 'cd sync && rake db:test:prepare && RAILS_ENV=test rake db:mongoid:create_indexes && xvfb-run rspec '"
-alias vscuke="vagrant ssh -c 'cd sync && rake db:test:prepare && RAILS_ENV=test rake db:mongoid:create_indexes && xvfb-run cucumber --tags ~@skip'"
+vsc () {
+    echo "starting $@ on vm"
+    time vagrant ssh -c "cd sync && $@"
+    echo "finished $@ on vm"
+}
+
+alias vsbundle="vsc bundle"
+alias vsrci="vsc 'rake db:mongoid:create_indexes'"
+alias vsrrad="vsc 'rails runner \"AdminUser.create(email: %q(development@bluewaterbrand.com), password: %q(password!!!), password_confirmation: %q(password!!!))\"'"
+alias vsrailsserv="vsc 'rails s -b 0.0.0.0'"
+alias vstestprep="vsc 'rake db:test:prepare'"
+alias vsspec="vsc 'xvfb-run rspec '"
+alias vscuke="vsc 'xvfb-run cucumber --tags ~@skip'"
+alias vstestall="vsspec && vscuke"
