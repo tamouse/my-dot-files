c70846dbf0d15714eca71e760f7974ad53e5bbeb
Tamara Temple <tamouse@gmail.com>
2017-03-18 21:29:34 -0500

    installed

--- profile.d/virt-env.sh
+++ profile.d/virt-env.sh
@@ -3,4 +3,4 @@
 # set up virtualenvwrapper for python3
 export WORKON_HOME=~/Envs
 export VIRTUALENVWRAPPER_PYTHON=`which python3`
-source /usr/local/bin/virtualenvwrapper.sh
+[ -x /usr/local/bin/virtualenvwrapper.sh ] && source /usr/local/bin/virtualenvwrapper.sh
