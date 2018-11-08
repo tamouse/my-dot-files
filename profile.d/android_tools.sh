57d851c0458aac6d0621c2703754fb52c091b3ca
Tamara Temple <tamouse@gmail.com>
2018-11-08 15:59:35 -0600

    fix broken variable
    Somehow deleted PATH off the end of PATH setting in android_tools.sh

--- profile.d/android_tools.sh
+++ profile.d/android_tools.sh
@@ -3,5 +3,5 @@
 # Assumes a symbolic link to the actual:
 : ${JAVA_HOME:=/Library/Java/JavaVirtualMachines/jdk1.8/Contents/Home}
 
-PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin:$
+PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin:$PATH"
 export PATH
