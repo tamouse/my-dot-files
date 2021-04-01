edd38c6f01c14c00fc002c5aeaa09e6476595de8
Tamara Temple <tamouse@gmail.com>
2021-04-01 15:58:08 -0500

    Add some more aliases n stuff
    - Add an editor config file for this repo as root
    - Add some aliases to speed up getting to work
    - Add some more git aliases

--- profile.d/dripdrv.sh
+++ profile.d/dripdrv.sh
@@ -3,6 +3,15 @@
 if [ -d $HOME/.drv ]; then
     export PATH=$PATH:$HOME/.drv/bin
     source $HOME/.drv/share/foundation-tools/bash_profile
+
+    # drip only akliases
+    alias .d="pushd $HOME/Work/Drip/drip/"
+    alias .drip="pushd $HOME/Work/Drip/drip/"
+    alias .de="pushd $HOME/Work/Drip/drip-elm/"
+    alias .dripelm="pushd $HOME/Work/Drip/drip-elm/"
+    alias .dm="pushd $HOME/Work/Drip/drip-data-mart-etl/"
+    alias .dmart="pushd $HOME/Work/Drip/drip-data-mart-etl/"
+
 else
     echo "No $HOME/.drv directory, not setting up or installing drip foundation tools"
 fi
