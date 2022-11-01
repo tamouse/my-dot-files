e1525cd116a3656bcfd0e7b69a7a11edc36e4c3d
Tamara Temple <tamouse@gmail.com>
2022-11-01 17:35:25 -0500

    Remove explicit things that can be set up in the working dir

--- zprofile.d/w4_aliases.zsh
+++ zprofile.d/w4_aliases.zsh
@@ -4,24 +4,3 @@ export WHEREFOUR_DIR="$HOME/Work/Wherefour"
 
 alias .w4a="pushd $WHEREFOUR_DIR/wherefour-app/"
 alias .w4devdocs="pushd $WHEREFOUR_DIR/Meta/DevDocs/"
-alias .reset="bundle exec rails db:migrate:reset"
-alias .load="bundle exec rails db:fixtures:load"
-alias .test="bundle exec rails test"
-alias .sytest="bundle exec rails test test/system"
-alias .brs="bundle exec rails server"
-alias .sk="bundle exec sidekiq | tee -a log/sidekiq.log"
-alias .ngk='ngrok http --log log/ngrok.log --log-format logfmt 3000'
-alias .byrl="bundle install && yarn install && bundle exec rails db:migrate:reset && bundle exec rails db:fixtures:load"
-
-## Testing
-export MUTE_TEST_AUDIO=true
-export NO_WHEREFOUR_REPORTER=1
-
-## Docker
-export DATABASE_USER=wherefour
-export DATABASE_PASSWORD=wherefour
-export DATABASE_HOST=postgresql
-
-## LeafLink
-export TUNNEL_LOCAL_HOST=''
-echo Remember to set TUNNEL_LOCAL_HOST after you have started ngrok!
