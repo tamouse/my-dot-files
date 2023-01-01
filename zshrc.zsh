d0beabd15e50df491d7f7fe0fa614c64e4e5cd30
Tamara Temple <tamouse@gmail.com>
2022-12-31 18:11:38 -0600

    Drop the local zsh-git-prompt

--- zshrc.zsh
+++ zshrc.zsh
@@ -100,3 +100,5 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 export PATH="/usr/local/opt/postgresql@10/bin:$PATH"
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
