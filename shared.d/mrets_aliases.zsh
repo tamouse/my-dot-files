67b01ca00dc5113bbf9bd21af60f4dc6f1b9dea3
Tamara Temple <tamara@mrets.org>
2023-10-03 10:35:49 -0500

    Update mrets-aliases
    - a little more goodness for restoring database dumps on docker
      - dcpgrestore function just does that, no fancy automation
      - dcpgsave_backup creates a backup database from the dev database as a tempoate
    
    - introduced an envar DEV_DB to keep from having to retype that odd construction

--- shared.d/mrets_aliases.zsh
+++ shared.d/mrets_aliases.zsh
@@ -6,6 +6,8 @@ export Work=$HOME/Work
 alias .m="pushd $Work/mrets/"
 alias .mr="pushd $Work/mrets-react/"
 
+export DEV_DB='m-rets_development'
+
 # Pull data from staging without enormous tables that aren't much use for dev/test
 # alias fetch_staging_dump="pg_dump --format=custom \$(heroku config:get DATABASE_URL --app m-rets-staging) --exclude-table-data='(wregis|mrets)_mat_*' --exclude-table-data='versions' --exclude-table-data='version_associations'"
 # alias fetch_rc_dump="pg_dump --format=custom \$(heroku config:get DATABASE_URL --app m-rets-rc) --exclude-table-data='(wregis|mrets)_mat_*' --exclude-table-data='versions' --exclude-table-data='version_associations'"
@@ -25,15 +27,15 @@ fetch_dump() {
 
 # quick drop and create dev database from template
 qk_reset() {
-  dropdb m-rets_development
-  createdb -T ${1} m-rets_development
+  dropdb "${DEV_DB}"
+  createdb -T ${1} "${DEV_DB}"
 }
 
 # refresh databawe from data pulled
 refresh_data() {
-  pg_restore --verbose --clean --no-acl --no-owner -d m-rets_development < ${1}
+  pg_restore --verbose --clean --no-acl --no-owner -d "${DEV_DB}" < ${1}
   bin/rails util:update_user_passwords_for_test PASSWORD=testtesttest
-  createdb --template m-rets_development ${2}_$(timestamp|tr '-' '_')
+  createdb --template "${DEV_DB}" ${2}_$(timestamp|tr '-' '_')
   psql -c '\l'
 }
 
@@ -65,25 +67,11 @@ alias dcdbmigrate='docker compose run --rm api /bin/bash -l -c "bundle exec rail
 alias dcbtestprepare='docker compose run --rm api /bin/bash -l -c "bundle exec rails db:test:prepare" '
 alias dcrspec='docker compose run --rm api /bin/bash -l -c "bundle exec rails db:test:prepare; bundle exec rspec" '
 
-alias dcpgdropdb="docker compose run --rm postgres_db /bin/bash -l -c 'pg_ctl start && dropdb m-rets_development && dropdb m-rets_test ; psql -c \"\l\" '"
-alias dcpgcreatedb="docker compose run --rm postgres_db /bin/bash -l -c 'pg_ctl start && createdb m-rets_development && createdb m-rets_test ; psql -c \"\l\" '"
-
+alias dcpgdropdb="docker compose run --rm postgres_db /bin/bash -l -c 'pg_ctl start && dropdb "${DEV_DB}" && dropdb m-rets_test ; psql -c \"\l\" '"
+alias dcpgcreatedb="docker compose run --rm postgres_db /bin/bash -l -c 'pg_ctl start && createdb "${DEV_DB}" && createdb m-rets_test ; psql -c \"\l\" '"
 dcpgrestore() {
-  dumpfile=${1}
-  base=$(basename $dumpfile)
-  name=$(basename $dumpfile .dump)
-  cp ${dumpfile} tmp/${base}
-  echo Dropping and creating data base
-  docker compose run --rm postgres_db /bin/bash -l -c 'pg_ctl start; dropdb m-rets_development; createdb m-rets_development; psql -c "\l"'
-  echo Restoring the data from the dump $dumpfile
-  docker compose run --rm postgres_db /bin/bash -l -c "pg_ctl start; pg_restore  --verbose --clean --no-acl --no-owner -d m-rets_development < app/tmp/${base}"
-  sed -i .bak -e 's/25/12/' lib/tasks/util/update_user_passwords_for_test.rake
-  git update-index --assume-unchanged lib/tasks/util/update_user_passwords_for_test.rake
-  echo Fixing up the passwords
-  docker compose run --rm api /bin/bash -l -c 'PASSWORD=testtesttest bundle exec rails util:update_user_passwords_for_test'
-  echo Making a backup db
-  docker compose run --rm postgres_db /bin/bash -l -c "pg_ctl start; createdb -T m-rets_development $name; psql -c \"\l\""
-  unset name
-  unset base
-  unset dumpfile
+  docker compose run --rm postgres_db /bin/bash -l -c "pg_ctl start && pg_restore --verbose --clean --no-acl --no-owner -d ${DEV_DB} ${1}"
+}
+dcpgsave_backup() {
+  docker compose run --rm postgres_db /bin/bash -l -c "pg_ctl start && createdb -T ${DEV_DB} ${1}"
 }
