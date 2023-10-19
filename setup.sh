8e72a48917a3939cdeef5222d0a3797cb5182cff
Tamara Temple <tamara@mrets.org>
2023-10-19 16:41:52 -0500

    Update some aliases, clean up some files

--- setup.sh
+++ setup.sh
@@ -54,26 +54,4 @@ for d in $SOURCE_DIRS ; do
     \cp -v "$d"/* "$HOME/.$(basename $d)/"
 done
 
-# ln -fs $PWD/bashrc $HOME/.bashrc
-# ln -fs $PWD/zshrc.zsh $HOME/.zshrc
-# ln -fs $PWD/gitconfig $HOME/.gitconfig
-# git config --global --add core.excludesfile $HOME/.gitignore_global
-# ln -fs $PWD/gitignore_global $HOME/.gitignore_global
-# # ln -fs $PWD/gradle.properties $HOME/.gradle.properties
-# # ln -fs $PWD/guard.rb $HOME/.guard.rb
-# ln -fs $PWD/hosts $HOME/.hosts
-# ln -fs $PWD/profile $HOME/.profile
-# ln -fs $PWD/zprofile $HOME/.zprofile
-# ln -fs $PWD/pryrc $HOME/.pryrc
-
-# # Get rid of these dirs before linking
-# mkdir -p $HOME/.old-dot-files
-# for d in $HOME/.bashrc.d $HOME/.profile.d $HOME/.shared.d $HOME/.zprofile.d; do
-#   [ -d $d ] && mv -vf $d $HOME/.old-dot-files && rm -vf $d
-# done
-# ln -fs $PWD/bashrc.d $HOME/.bashrc.d
-# ln -fs $PWD/profile.d $HOME/.profile.d
-# ln -fs $PWD/shared.d $HOME/.shared.d
-# ln -fs $PWD/zprofile.d $HOME/.zprofile.d
-
 echo "Log out and log in again to ensure everything is in it's place."
